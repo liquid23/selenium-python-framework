f339cd5f0e672813d77af7381b71fa97e54dca92
Sumit <aryasumit999@gmail.com>
2026-03-23 14:22:38 +0530

    updated to headless

--- utils/driver_setup.py
+++ utils/driver_setup.py
@@ -10,5 +10,5 @@ def get_driver():
     options.add_argument("--no-sandbox")
     options.add_argument("disable-dev-shm-usage")
 
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()) options=options)
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
     return driver
